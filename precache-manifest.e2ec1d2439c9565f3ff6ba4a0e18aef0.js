80f45ca7cb1b669429d9ecdebf0aa4e4e29a0265
Sentenzos <49534576+Sentenzos@users.noreply.github.com>
2020-04-23 20:27:04 +0300

    Updates

---
+++ precache-manifest.e2ec1d2439c9565f3ff6ba4a0e18aef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828840398055c3bff75dc96898afc962",
+    "url": "/path-and-window/index.html"
+  },
+  {
+    "revision": "b89834a401c285b9eb36",
+    "url": "/path-and-window/static/css/main.c3efeb01.chunk.css"
+  },
+  {
+    "revision": "eaf513d455143f2d230e",
+    "url": "/path-and-window/static/js/2.e2a40fb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/path-and-window/static/js/2.e2a40fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89834a401c285b9eb36",
+    "url": "/path-and-window/static/js/main.37c58ef1.chunk.js"
+  },
+  {
+    "revision": "b0eed67eec8bcce408f6",
+    "url": "/path-and-window/static/js/runtime-main.33862d28.js"
+  }
+]);
